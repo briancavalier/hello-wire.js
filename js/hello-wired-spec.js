795d9eec8ca6730e6644c3f5a567a0fffe7a1b9c
Brian Cavalier <brian@briancavalier.com>
2011-05-17 09:49:25 -0400

    Adding docco-style comments in preparation for pushing some docs

--- js/hello-wired-spec.js
+++ js/hello-wired-spec.js
@@ -1,14 +1,38 @@
+// This is a [wire.js](https://github.com/briancavalier/wire) wiring spec
+// for the simple [hello-wire](https://github.com/briancavalier/hello-wire)
+// example.  For more information, check out the [README](https://github.com/briancavalier/hello-wire),
+// and the [wire.js docs](https://github.com/briancavalier/wire/wiki)
+
+// Standard AMD module define() wrapper
 define({
+	// A regular String.  Basic Javascript types are supported directly
+	// in wiring specs, even non-primitives like Date and RegExp.
 	message: "I haz been wired",
+	
+	// Create an instance of the hello-wired module.
 	helloWired: {
+
+		// The hello-world module returns a constructor function, which
+		// wire.js will call to create the instance, passing a single
+		// parameter, the DOM Node whose id is "hello".  This uses
+		// JSON Reference syntax along with the `dom!` resolver provided
+		// by the `wire/dom` plugin below.
 		create: {
 			module: 'hello-wired',
 			args: { $ref: 'dom!hello' }
 		},
+		
+		// Invoke the sayHello method on the instance after it is
+		// created, and pass a single parameter, the message String
+		// defined above.  Again, you can use JSON Reference syntax to
+		// reference other objects in the wiring spec.
 		init: {
 			sayHello: { $ref: 'message' }
 		}
 	},
+	
+	// Load the basic wire.js dom plugin, which provides the `dom!`
+	// resolver used above.
 	plugins: [
 		{ module: 'wire/dom' }
 	]
--- js/hello-wired.js
+++ js/hello-wired.js
@@ -1,13 +1,25 @@
+// A simple module that defines the HelloWired "class", i.e. a constructor
+// with a prototype, that has a single method, sayHello.  For more
+// information, check out the [README](https://github.com/briancavalier/hello-wire),
+// and the [wire.js docs](https://github.com/briancavalier/wire/wiki)
+
+// Standard AMD define wrapper
 define([], function() {
+	
+	// The constructor takes a single parameter, the DOM Node in which it
+	// will render the message passed to sayHello.
 	function HelloWired(node) {
 		this._node = node;
 	}
 	
 	HelloWired.prototype = {
+		// The sayHello method takes a message String and renders it to
+		// the DOM Node that was supplied to the constructor.
 		sayHello: function(message) {
 			this._node.innerHTML = "Hello! " + message;
 		}
 	};
 	
+	// Return the constructor
 	return HelloWired;
 });
